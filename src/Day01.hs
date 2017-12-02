d3262cbcfa7a9e260b218a4ca1751789c1bf011b
Brad Chase <brad@these8bits.com>
2017-12-02 05:57:49 -0500

    mv logic to Day01 module

---
+++ src/Day01.hs
@@ -0,0 +1,20 @@
+module Day01
+  ( day01a
+  , day01b
+  ) where
+
+
+day01a :: String -> Int
+day01a = day01 1
+
+day01b :: String -> Int
+day01b str = day01 (length str `quot` 2) str
+
+day01 :: Int -> String -> Int
+day01 _ "" = 0
+day01 times str =
+  let nums = map ((read :: String -> Int) . return) $ str
+   in sum . map fst . filter (\(n0,n1) -> n0 == n1) . zip (rotate times nums) $ nums
+  where
+    rotate :: Int -> [a] -> [a]
+    rotate t xs = (drop t xs) ++ (take t xs)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,20 +1,5 @@
 module Lib
-    ( day01a
-    , day01b
+    ( module Day01
     ) where
 
-
-day01a :: String -> Int
-day01a = day01 1
-
-day01b :: String -> Int
-day01b str = day01 (length str `quot` 2) str
-
-day01 :: Int -> String -> Int
-day01 _ "" = 0
-day01 times str =
-  let nums = map ((read :: String -> Int) . return) $ str
-   in sum . map fst . filter (\(n0,n1) -> n0 == n1) . zip (rotate times nums) $ nums
-  where
-    rotate :: Int -> [a] -> [a]
-    rotate t xs = (drop t xs) ++ (take t xs)
+import Day01
