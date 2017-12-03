1d89b528caa2d56918f6e2ac2bef92557256e5db
Brad Chase <brad@these8bits.com>
2017-12-02 20:44:27 -0500

    fix Day01; run Day01 tests

--- test/AocSpec.hs
+++ test/AocSpec.hs
@@ -17,24 +17,24 @@ spec = do
       it "generates a checksum of the input" $ do
         (day02b input) `shouldBe` 9
 
-  -- describe "Day01" $ do
-  --   describe "Day01A" $ do
-  --     it "produces 3 for 1122" $ do
-  --       (day01a "1122") `shouldBe` 3
-  --     it "produces 4 for 1111" $ do
-  --       (day01a "1111") `shouldBe` 4
-  --     it "produces 0 for 1234" $ do
-  --       (day01a "1234") `shouldBe` 0
-  --     it "produces 9 for 91212129" $ do
-  --       (day01a "91212129") `shouldBe` 9
-  --   describe "Day01B" $ do
-  --     it "produces 6 for 1212" $ do
-  --       (day01b "1212") `shouldBe` 6
-  --     it "produces 0 for 1221" $ do
-  --       (day01b "1221") `shouldBe` 0
-  --     it "produces 4 for 123425" $ do
-  --       (day01b "123425") `shouldBe` 4
-  --     it "produces 12 for 123123" $ do
-  --       (day01b "123123") `shouldBe` 12
-  --     it "produces 4 for 12131415" $ do
-  --       (day01b "12131415") `shouldBe` 4
+  describe "Day01" $ do
+    describe "Day01A" $ do
+      it "produces 3 for 1122" $ do
+        (day01a "1122") `shouldBe` 3
+      it "produces 4 for 1111" $ do
+        (day01a "1111") `shouldBe` 4
+      it "produces 0 for 1234" $ do
+        (day01a "1234") `shouldBe` 0
+      it "produces 9 for 91212129" $ do
+        (day01a "91212129") `shouldBe` 9
+    describe "Day01B" $ do
+      it "produces 6 for 1212" $ do
+        (day01b "1212") `shouldBe` 6
+      it "produces 0 for 1221" $ do
+        (day01b "1221") `shouldBe` 0
+      it "produces 4 for 123425" $ do
+        (day01b "123425") `shouldBe` 4
+      it "produces 12 for 123123" $ do
+        (day01b "123123") `shouldBe` 12
+      it "produces 4 for 12131415" $ do
+        (day01b "12131415") `shouldBe` 4
