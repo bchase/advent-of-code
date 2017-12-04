8c8ace476407c80438d3cd4a8cc7cc8840d4bd74
Brad Chase <brad@these8bits.com>
2017-12-04 04:13:14 -0500

    impl Day04 A & B

--- haskell/src/Year2017.hs
+++ haskell/src/Year2017.hs
@@ -2,8 +2,10 @@ module Year2017
     ( module Year2017.Day01
     , module Year2017.Day02
     , module Year2017.Day03
+    , module Year2017.Day04
     ) where
 
 import           Year2017.Day01
 import           Year2017.Day02
 import           Year2017.Day03
+import           Year2017.Day04
---
+++ haskell/src/Year2017/Day04.hs
@@ -0,0 +1,29 @@
+module Year2017.Day04 ( day04 ) where
+
+import           Data.List.Unique (unique)
+import qualified Data.Map.Strict  as Map
+import           Data.Map.Strict  (Map)
+
+
+day04 :: String -> IO [String]
+day04 passphrase = do
+  -- return [show . length . filter id . map hasNoDuplicateTokens . lines $ ps] -- PART 1
+  -- return [show . length . filter id . map hasNoAnagramTokens . lines $ ps]   -- PART 2
+  return $ map show [ hasNoDuplicateTokens passphrase
+                    , hasNoAnagramTokens passphrase
+                    ]
+
+hasNoDuplicateTokens :: String -> Bool
+hasNoDuplicateTokens = isUniqueList . words
+
+hasNoAnagramTokens :: String -> Bool
+hasNoAnagramTokens = isUniqueList . map getCharCounts . words
+  where
+    getCharCounts :: String -> [(String, Int)]
+    getCharCounts = Map.toList . foldr incCharCount (Map.fromList [])
+
+    incCharCount :: Char -> Map String Int -> Map String Int
+    incCharCount ch m = let n = Map.findWithDefault 0 [ch] m in Map.insert [ch] (n+1) m
+
+isUniqueList :: (Ord a) => [a] -> Bool
+isUniqueList ps = length ps == length (unique ps)
