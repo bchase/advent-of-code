151211a39ab3726302a3027eae28fb5106430969
Brad Chase <brad@these8bits.com>
2017-12-05 04:40:24 -0500

    impl Haskell-style Day04 (test) in Elm

--- elm/src/Year2017/Day04.elm
+++ elm/src/Year2017/Day04.elm
@@ -1,14 +1,46 @@
 module Year2017.Day04 exposing (..)
 
+import String as S
+import List as L
+import List.Extra as L
 import Types exposing (Flags, Input, Model, Mode(..), AB(..), Output)
 import Helpers
+
+
+(.) =
+  (<<)
+
+
+($) =
+  (<|)
+
+
+noDuplicateTokens : String -> Bool
+noDuplicateTokens =
+  allTokensUniq . S.words
+
+
+noDuplicateAnagramTokens : String -> Bool
+noDuplicateAnagramTokens =
+  allTokensUniq . L.map (S.join "" . L.sort . S.split "") . S.words
+
+
+allTokensUniq : List String -> Bool
+allTokensUniq tokens =
+  L.length tokens == L.length (L.unique tokens)
 
 
 solve : Input -> String
 solve { mode, ab, input } =
   case ( mode, ab ) of
+    ( Test, A ) ->
+      toString $ noDuplicateTokens input
+
+    ( Test, B ) ->
+      toString $ noDuplicateAnagramTokens input
+
     ( _, _ ) ->
-      (\str -> "received input: " ++ str) input
+      (\str -> "NOT-IMPLEMENTED -- input: " ++ str) input
 
 
 main =
