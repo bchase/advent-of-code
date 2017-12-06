650168103f3cb83fa04dcd50facfdf8e2f6fd207
Brad Chase <brad@these8bits.com>
2017-12-06 07:34:55 -0500

    impl Day06B Haskell

--- haskell/src/Year2017/Day06.hs
+++ haskell/src/Year2017/Day06.hs
@@ -11,59 +11,71 @@ type Block = (Idx, Val)
 type Idx = Int
 type Val = Int
 
+type State = (Idx, MemoryBank)
+type History = (State, [State])
+
 
 day06 :: Mode -> AB -> String -> IO [String]
-day06 mode ab input = return $ [ result ]
+day06 mode ab input = return [ result ]
   where
     result =
       case (mode, ab) of
         (Test, A) -> show . map snd . redistribute . parse $ input
-        (Run,  A) -> do
-          -- let mb = parse "[0,2,7,0]"
-          let mb = parse "[0,5,10,0,11,14,13,4,11,8,8,7,1,4,12,11]"
-          show . length . snd . until stateHasBeenSeenBefore redistributeAndTrack $ (mb, [])
-
-        _ -> error "NOTIMPL"
+        (Run,  A) -> do -- let mb = parse "[0,2,7,0]" -- ==> 5
+          let mb = parse "[0,5,10,0,11,14,13,4,11,8,8,7,1,4,12,11]" -- ==> 7864
+          show $ stepsUntilStateHasBeenSeenBefore mb
+        (Run,  B) -> do -- let mb = parse "[0,2,7,0]" -- ==> 4
+          let mb = parse "[0,5,10,0,11,14,13,4,11,8,8,7,1,4,12,11]" -- ==> 1695
+          show $ stepsBackToFirstPreviouslySeenState mb
+        _ -> error "NOT-IMPLEMENTED"
 
 
 parse :: String -> MemoryBank
 parse = zip [0..] . read
 
-stateHasBeenSeenBefore :: (MemoryBank, [MemoryBank]) -> Bool
-stateHasBeenSeenBefore (mb, mbs) = mb `elem` mbs
-redistributeAndTrack :: (MemoryBank, [MemoryBank]) -> (MemoryBank, [MemoryBank])
-redistributeAndTrack (mb, mbs) = let mb' = redistribute mb in (mb', mb:mbs)
+stepsUntilStateHasBeenSeenBefore :: MemoryBank -> Int
+stepsUntilStateHasBeenSeenBefore =
+  length . snd . redistributeUntilStateIsRepeated
+
+stepsBackToFirstPreviouslySeenState :: MemoryBank -> Int
+stepsBackToFirstPreviouslySeenState mb =
+  let ((curr, mb'), mbs) = redistributeUntilStateIsRepeated mb
+      prev = fromJust $ fmap fst . find ((==) mb' . snd) $ mbs
+   in curr - prev
+
+redistributeUntilStateIsRepeated :: MemoryBank -> History
+redistributeUntilStateIsRepeated =
+  until stateHasBeenSeenBefore redistributeAndTrack . emptyHistory
+  where
+    emptyHistory mb = ((0, mb), [])
+    stateHasBeenSeenBefore ((_, mb), mbs) = mb `elem` map snd mbs
+    redistributeAndTrack((itr, mb), mbs) =
+      let mb' = redistribute mb in ((itr+1, mb'), (itr, mb):mbs)
+
 
 redistribute :: MemoryBank -> MemoryBank
-redistribute bank =
-  let (i, val', dist) = blockToRedistribute bank
-   in setBlockAndRedistributeVal (i,val') dist bank
+redistribute = zeroBlockAndRedistributeVal . blockToRedistribute
   where
-    blockToRedistribute :: MemoryBank -> (Idx, Val, Val)
+    blockToRedistribute :: MemoryBank -> (Block, MemoryBank)
     blockToRedistribute mb = do
       let max' = maximum . map snd $ mb
-          blks = filter (flip (==) max' . snd) mb
-      case blks of
-        -- [(idx,val)] -> (idx, 1, val-1)
-        [(idx,val)] -> (idx, 0, val)
-        (idx,val):_ -> (idx, 0, val)
-        _           -> error "EMPTY-MemoryBank"
+       in (head . filter (flip (==) max' . snd) $ mb, mb)
 
-    setBlockAndRedistributeVal :: Block -> Int -> MemoryBank -> MemoryBank
-    setBlockAndRedistributeVal (ignore, val') dist mb =
-      let mb' = setValAtIdx ignore val' mb
-       in (\(_,_,r,_) -> r) . until blockRedistributed incNextBlockVal $ (ignore, dist, mb', ignore)
+    zeroBlockAndRedistributeVal :: (Block, MemoryBank) -> MemoryBank
+    zeroBlockAndRedistributeVal ((idx, dist), mb)=
+      let mb' = setValAtIdx idx 0 mb
+          trd (_,_,a) = a
+       in trd . until blockRedistributed incNextBlockVal $ (idx, dist, mb')
 
-    blockRedistributed :: (Idx, Int, MemoryBank, Idx) -> Bool
-    blockRedistributed (_, 0, _, _) = True
+    blockRedistributed :: (Idx, Int, MemoryBank) -> Bool
+    blockRedistributed (_, 0, _) = True
     blockRedistributed _ = False
 
-    incNextBlockVal :: (Idx, Int, MemoryBank, Idx) -> (Idx, Int, MemoryBank, Idx)
-    incNextBlockVal (idx, dist, mb, ignore) = do
+    incNextBlockVal :: (Idx, Int, MemoryBank) -> (Idx, Int, MemoryBank)
+    incNextBlockVal (idx, dist, mb) = do
       let (idx',val) = nextBlock (idx+1) mb
-          -- (mb', dist') = if idx' == ignore then (mb, dist) else (setValAtIdx idx' (val+1) mb, dist-1)
-          (mb', dist') = if False then (mb, dist) else (setValAtIdx idx' (val+1) mb, dist-1)
-       in (idx', dist', mb', ignore)
+          (mb', dist') = (setValAtIdx idx' (val+1) mb, dist-1)
+       in (idx', dist', mb')
 
     nextBlock :: Idx -> MemoryBank -> Block
     nextBlock idx mb =
