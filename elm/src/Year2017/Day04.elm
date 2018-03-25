0724e5aeda09b7b48d04542809abb47937b229a0
Brad Chase <brad@these8bits.com>
2018-03-24 22:07:04 -0400

    complete solution for 2017-04 in elm

--- elm/src/Year2017/Day04.elm
+++ elm/src/Year2017/Day04.elm
@@ -7,22 +7,14 @@ import Types exposing (Input, Mode(..), AB(..))
 import Helpers exposing (defaultMain)
 
 
-(.) =
-  (<<)
-
-
-($) =
-  (<|)
-
-
 noDuplicateTokens : String -> Bool
 noDuplicateTokens =
-  allTokensUniq . S.words
+  allTokensUniq << S.words
 
 
 noDuplicateAnagramTokens : String -> Bool
 noDuplicateAnagramTokens =
-  allTokensUniq . L.map (S.join "" . L.sort . S.split "") . S.words
+  allTokensUniq << L.map (S.join "" << L.sort << S.split "") << S.words
 
 
 allTokensUniq : List String -> Bool
@@ -32,15 +24,30 @@ allTokensUniq tokens =
 
 solve : Input -> String
 solve { mode, ab, input } =
-  case ( mode, ab ) of
-    ( Test, A ) ->
-      toString $ noDuplicateTokens input
+  let
+    ps =
+      input
+        |> S.split "\\n"
+        |> L.filter (\p -> p /= "")
+  in
+    case ( mode, ab ) of
+      ( Test, A ) ->
+        toString <| noDuplicateTokens input
 
-    ( Test, B ) ->
-      toString $ noDuplicateAnagramTokens input
+      ( Test, B ) ->
+        toString <| noDuplicateAnagramTokens input
 
-    ( _, _ ) ->
-      (\str -> "NOT-IMPLEMENTED -- input: " ++ str) input
+      ( Run, A ) ->
+        ps
+          |> L.filter noDuplicateTokens
+          |> L.length
+          |> toString
+
+      ( Run, B ) ->
+        ps
+          |> L.filter noDuplicateAnagramTokens
+          |> L.length
+          |> toString
 
 
 main =
