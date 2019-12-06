50e55ba7de4707082ad258e04798bf81cbb956ae
Brad Chase <brad@these8bits.com>
2019-12-06 17:15:08 -0500

    impl 2019 Day01.A

--- 2019/elm/src/Day01/A.elm
+++ 2019/elm/src/Day01/A.elm
@@ -1,4 +1,4 @@
-module Day01.A exposing (main, solve)
+module Day01.A exposing (main, solve, requiredFuelForMass)
 
 import Helpers exposing (wrap)
 
@@ -7,4 +7,27 @@ main = wrap solve
 
 
 solve : String -> String
-solve = identity
+solve input =
+  input
+    |> String.split "\n"
+    |> List.filter (\ str -> not (str == "") )
+    |> List.map (String.toInt >> fromJust)
+    |> List.map requiredFuelForMass
+    |> List.sum
+    |> String.fromInt
+
+
+requiredFuelForMass : Int -> Int
+requiredFuelForMass mass =
+  toFloat mass / toFloat 3
+    |> floor
+    |> ((-) 2)
+    |> negate
+  -- div 3, round down, sub 2
+
+
+fromJust : Maybe a -> a
+fromJust mx = case mx of
+  Just x  -> x
+  Nothing -> Debug.todo ""
+
--- 2019/elm/tests/Test/Day01.elm
+++ 2019/elm/tests/Test/Day01.elm
@@ -8,18 +8,29 @@ import Day01.B as B
 
 
 
+-- For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2
+
 
 suite : Test
 suite =
   describe "Day01"
     [ describe "A"
-      [ test "something" <| \_ ->
-        A.solve "123"
-          |> Expect.equal "123"
+      [ test "requiredFuelForMass 1" <| \_ ->
+        A.requiredFuelForMass 12
+          |> Expect.equal 2
+      , test "requiredFuelForMass 2" <| \_ ->
+        A.requiredFuelForMass 14
+          |> Expect.equal 2
+      , test "requiredFuelForMass 3" <| \_ ->
+        A.requiredFuelForMass 1969
+          |> Expect.equal 654
+      , test "requiredFuelForMass 4" <| \_ ->
+        A.requiredFuelForMass 100756
+          |> Expect.equal 33583
       ]
-    , describe "B"
-      [ test "something" <| \_ ->
-        B.solve "123"
-          |> Expect.equal "123"
-      ]
+    -- , describe "B"
+    --   [ test "something" <| \_ ->
+    --     B.solve "123"
+    --       |> Expect.equal "123"
+    --   ]
     ]
